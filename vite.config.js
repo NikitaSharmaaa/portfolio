e6301c0419c6d344082547f2df78d52ff9d98f27
NikitaSharmaaa <150362410+NikitaSharmaaa@users.noreply.github.com>
2026-06-08 11:18:22 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
   plugins: [react()],
+  base: '/portfolio/',
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
